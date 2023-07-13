ec5797e4edacec10f0cd83fb94a9b042578878e3
Ana Julia M Martins <ana_juh17@hotmail.com>
2023-07-13 18:08:46 -0300

    múltiplas requisições

--- Assets/JS/main.js
+++ Assets/JS/main.js
@@ -1,20 +1,18 @@
-//Transformar no elemento li de HTML
 function convertToHtmlLi(pokemon){
-    return `<li class="pokemon">
-        <span class="number">#001</span>
-        <span class="name">${pokemon.name}</span>
-        <div class="detail">
-            <ol class="types">
-                <li class="type">grass</li>
-                <li class="type">posion</li>
-            </ol>
-            <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg" alt="${pokemon.name}">
-        </div>
+    return `
+        <li class="pokemon ${pokemon.type}">
+            <span class="number">${'#'+pokemon.number}</span>
+            <span class="name">${pokemon.name}</span>
+            <div class="detail">
+                <ol class="types">
+                    ${pokemon.types.map((type)=>`<li class="type">${type}</li>`).join('')}
+                </ol>
+                <img src="${pokemon.image}" alt="${pokemon.name}">
+            </div>
         </li>`
 }
 
 const pokemonList = document.getElementById('pokemonList')
-
 async function manipulateHtml(){
     const pokemons = await pokeApi.getPokemons();     //pokeAPI é um objeto que vem do script 'poke-api.js'
 
--- Assets/JS/poke-api.js
+++ Assets/JS/poke-api.js
@@ -1,21 +1,46 @@
 const pokeApi = {}
 
+function createPokemonCustomDescription(pokeApiDetails){
+    const pokemon = new Pokemon();
+    pokemon.name = pokeApiDetails.name;
+    pokemon.number = pokeApiDetails.order;
+    pokemon.image = pokeApiDetails.sprites.other.dream_world.front_default;
+
+
+    const types = pokeApiDetails.types.map((typeSlot)=> typeSlot.type.name);
+    const {type} = types //destructing, equivalente a fazer lista.get(0), lista.get(1), lista.get(2)
+
+    pokemon.types = types
+    pokemon.type = type
+
+    return pokemon;
+}
+
+
+pokeApi.getPokemonDetail = async (pokemon)=>{
+    const pokemonUrl = await fetch(pokemon.url)
+    const pokemonUrlJson = await pokemonUrl.json();
+    return createPokemonCustomDescription(pokemonUrlJson)
+
+}
+
 //Consumo da API
 pokeApi.getPokemons = async (offset = 0, limit = 10)=>{
     try{
         const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`;
-
         const response = await fetch(url);
         const responseJson = await response.json();
-        
-        return responseJson.results;
+
+        const pokemonList = responseJson.results;
+        const pokemonUrls = await pokemonList.map(pokeApi.getPokemonDetail)
+        console.log(pokemonUrls);
+        const pokemonDetail = await Promise.all(pokemonUrls);
+        console.log(pokemonDetail);
+         
+        return pokemonDetail;
+     
     }
     catch(error){
         console.log('Erro ao buscar os pokémons:', error);
     }
 }
-
-
-const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`;
-
-fetch(url);
